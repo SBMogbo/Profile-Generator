83348baab7ad8cf8c65482bd2a140181212e46b0
Steven Brooks Mogbo <stevenmogbo@yahoo.com>
2020-09-02 19:13:25 -0400

    first commit

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,9 @@
+// TODO: Write code to define and export the Employee class
+
+var fs=require("fs");
+const inquirer=require("inquirer");
+//const util=require("util");
+//const generateMarkdown=require("./utils/generateMarkdown")
+
+
+
